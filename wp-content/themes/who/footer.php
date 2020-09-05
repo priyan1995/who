86ea1f8038de04ab87a475084bea08484164db3d
priyan <45042030+priyan1995@users.noreply.github.com>
2020-09-05 22:09:29 +0530

    footer menu added

--- wp-content/themes/who/footer.php
+++ wp-content/themes/who/footer.php
@@ -1,23 +1,31 @@
-
 <footer class="site-footer bg-light-blue pd-footer">
 	<div class="site-footer__inner section section--large">
 		<ul class="social">
 			<li class="social__item social__item--facebook">
 				<a href="#" target="_blank">
-				<img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/facebook.svg"></a>
+					<img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/facebook.svg"></a>
 			</li>
 
 			<li class="social__item social__item--twitter">
 				<a href="#" target="_blank">
-				<img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/twitter.svg"></a>
+					<img src="<?php echo bloginfo('template_url'); ?>/assets/img/icons/twitter.svg"></a>
 			</li>
 		</ul>
 
-		<ul class="site-footer__menu">
-			<li><a href="#">Terms &amp; conditions</a></li>
-			<li><a href="#">Privacy policy</a></li>
-			<li><a href="#">WHO website</a></li>
-		</ul>
+		<?php
+		wp_nav_menu(
+			array(
+				'depth' => '10',
+				'theme_location' => 'footer',
+				'container_class' => '',
+				'menu_class' => 'site-footer__menu',
+				'menu_id' => 'footer',
+				'walker' => new WP_Bootstrap_Navwalker(),
+
+			)
+		);
+
+		?>
 
 		<div class="site-footer__copy">
 			<p class="txt-small">&copy; 2017 WHO. All Rights Reserved</p>
@@ -31,4 +39,5 @@
 
 <?php wp_footer();  ?>
 </body>
+
 </html>
